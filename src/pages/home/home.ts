29072fc63b1dba9926cc0ba92508d27f6acf8830
kobaska <kogulan.baskaran@gmail.com>
2019-06-13 11:28:26 +0100

    New plugin for iOS bluetooth connection

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,68 +1,44 @@
-import { ElementRef, Component, NgZone, HostListener, OnInit } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from '@angular/core';
 import { AlertController, LoadingController, ToastController } from 'ionic-angular';
 import { Platform } from 'ionic-angular';
+
+export interface DeviceConfig {
+  address: number;
+  name: string;
+  id: string;
+  protocols: string[];
+}
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage implements OnInit {
-  @HostListener('input', ['$event.target'])
-  onInput(textArea: HTMLTextAreaElement): void {
-    this.adjust();
-  }
+export class HomePage {
+  private readonly CARRIAGE_DATA: string = '{ZC1}';
+  public message: String = '';
 
-  output: any;
-  message: String = '';
-  responseTxt: any;
-  unpairedDevices: any;
-  pairedDevices: any;
-  statusMessage: string;
-  gettingDevices: Boolean;
+  public pairedDevices: DeviceConfig[];
+  public pairedDeviceId: number = 0;
+  public weigherConfig: DeviceConfig;
 
-  pairedList: pairedlist;
-  listToggle: boolean = false;
-  pairedDeviceID: number = 0;
-  dataSend: string = '';
-
-
-  showConnectBarcode: Boolean = true;
-  showDisconnectBarcode: Boolean = false;
-  showConnectReader: Boolean = true;
-  showDisconnectReader: Boolean = false;
-  showConnectWeigher: Boolean = true;
-  showDisconnectWeigher: Boolean = false;
-
-  address1: string;
-  address2: string;
-  address3: string;
   private bluetoothSerial;
 
   constructor(
     public loadCtrl: LoadingController,
     private alertCtrl: AlertController,
-    public navCtrl: NavController,
-    private ngZone: NgZone,
     private platform: Platform,
-    private toastCtrl: ToastController,
-    public element: ElementRef
+    private toastCtrl: ToastController
   ) {
     this.platform.ready().then(() => {
-      this.bluetoothSerial = (<any>window.bluetoothSerial);
-
+      this.bluetoothSerial = (<any>window).bluetoothClassicSerial;
       this.checkBluetoothEnabled();
     });
-  }
-
-  ngOnInit(): void {
-    setTimeout(() => this.adjust(), 0);
   }
 
   checkBluetoothEnabled() {
     this.bluetoothSerial.isEnabled(
       success => {
-        this.listPairedDevices();
+        this.refreshPairedDevices();
       },
       error => {
         this.showError('Please Enable Bluetooth');
@@ -70,333 +46,209 @@ export class HomePage implements OnInit {
     );
   }
 
-  listPairedDevices() {
+  refreshPairedDevices() {
+    console.log('listing bluetooth devices');
+
     this.bluetoothSerial.list(
-      success => {
-        this.pairedList = success;
-        this.listToggle = true;
+      pairedDevices => {
+        console.log('list of bluetooth devices');
+
+        for (let device of pairedDevices) {
+          console.log('device', device);
+        }
+
+        this.pairedDevices = pairedDevices;
       },
       error => {
+        console.log('error listing bluetooth devices', error);
         this.showError('Please Enable Bluetooth');
-        this.listToggle = false;
       }
     );
   }
 
-  
-  /* Select Devices */ 
-  selectDevice() {
-    this.showConnectReader = false;
-    this.showDisconnectReader = true;
+  connectUnpairedDevice() {
+    this.bluetoothSerial.discoverUnpaired(
+      success => {
+        console.log('list of bluetooth discoverUnpaired devices', success);
 
-    let connectedDevice = this.pairedList[this.pairedDeviceID];
+        for (let device of success) {
+          console.log('discoverUnpaired device', device);
+        }
 
-    if (!connectedDevice.address) {
-      this.showError('Select Paired Device to connect');
-      return;
-    }
-    let address = connectedDevice.address;
-    this.address1 = address;
-
-    this.connect(address);
+        this.pairedDevices = success;
+      },
+      error => {
+        console.log('error listing bluetooth devices', error);
+        this.showError('Please Enable Bluetooth');
+      }
+    );
   }
 
-  selectDevice2() {
-    this.showConnectWeigher = false;
-    this.showDisconnectWeigher = true;
+  /* Connect Devices */
+  connectDevice(weigher = false) {
+    console.log('connect device called', weigher);
+    const connectedDevice = this.pairedDevices[this.pairedDeviceId];
+    const config: DeviceConfig = {
+      id: connectedDevice.id,
+      name: connectedDevice.name,
+      address: connectedDevice.address,
+      protocols: connectedDevice.protocols.filter(p => p !== '')
+    };
 
-    let connectedDevice = this.pairedList[this.pairedDeviceID];
-    if (!connectedDevice.address) {
-      this.showError('Select Paired Device to connect');
-      return;
+    if (weigher) {
+      this.weigherConfig = config;
+      this.connect(config, weigher);
+    } else {
+      this.connect(config, weigher);
     }
-    let address = connectedDevice.address;
-    this.address2 = address;
-
-    this.connect2(address);
   }
-
-  selectDevice3() {
-    this.showConnectBarcode = false;
-    this.showDisconnectBarcode = true;
-
-    let connectedDevice = this.pairedList[this.pairedDeviceID];
-    if (!connectedDevice.address) {
-      this.showError('Select Paired Device to connect');
-      return;
-    }
-    let address = connectedDevice.address;
-    this.address3 = address;
-
-    this.connect3(address);
-  }
-
 
   /* Connect */
-  connect(address) {
+  connect(config: DeviceConfig, weigher = false) {
+    const address = config.address;
     // Attempt to connect device with specified address, call app.deviceConnected if success
-    console.log('This is the connected address:', address);
+    console.log('This is the connected address:', config);
     this.showToast(`this is the connected address ${address}`);
 
-    this.bluetoothSerial.connect(
-      address,
-      success => {
-        this.deviceConnected(address,
-          );
-        this.showToast(`Successfully Connected  ${address}`);
-        console.log('Success address:', address);
-      },
-      error => {
-        this.showError(`Error:Connecting to Device  ${address}`);
-        console.log('Failure address:', address);
-      }
-    );
+    console.log('config', config.id, config.protocols);
+
+    if (weigher) {
+      this.bluetoothSerial.connect(
+        config.id,
+        config.protocols.filter(p => p !== ''),
+        success => {
+          console.log('Success address:', success);
+          this.showToast(`Successfully Connected  ${address}`);
+          this.deviceConnected(config.protocols[0], weigher);
+        },
+        error => {
+          this.showError(`Error:Connecting to Device  ${address}`);
+          console.log('error', error);
+        }
+      );
+    } else {
+      this.bluetoothSerial.connect(
+        config.id,
+        config.protocols.filter(p => p !== ''),
+        success => {
+          console.log('Success address:', success);
+          this.showToast(`Successfully Connected  ${address}`);
+          this.deviceConnected(config.protocols[0], weigher);
+        },
+        error => {
+          this.showError(`Error:Connecting to Device  ${address}`);
+          console.log('error', error);
+        }
+      );
+    }
   }
-
-  connect2(address) {
-    // Attempt to connect device with specified address, call app.deviceConnected if success
-    console.log('This is the connected address:', address);
-    this.showToast(`this is the connected address ${address}`);
-
-    this.bluetoothSerial.connect2(
-      address,
-      success => {
-        this.deviceConnected2(address,
-          );
-        this.showToast(`Successfully Connected  ${address}`);
-        console.log('Success address:', address);
-      },
-      error => {
-        this.showError(`Error:Connecting to Device  ${address}`);
-        console.log('Failure address:', address);
-      }
-    );
-  }
-
-  connect3(address) {
-    // Attempt to connect device with specified address, call app.deviceConnected if success
-    console.log('This is the connected address:', address);
-    this.showToast(`this is the connected address ${address}`);
-
-    this.bluetoothSerial.connect3(
-      address,
-      success => {
-        this.deviceConnected3(address,
-          );
-        this.showToast(`Successfully Connected  ${address}`);
-        console.log('Success address:', address);
-      },
-      error => {
-        this.showError(`Error:Connecting to Device  ${address}`);
-        console.log('Failure address:', address);
-      }
-    );
-  }
-
 
   /* Device Connected */
-  deviceConnected(address) {
-    // Subscribe to data receiving as soon as the delimiter is read
-    this.bluetoothSerial.subscribe(
-      '\r',
-      success => {
-        var bytes = new Uint8Array(success);
+  deviceConnected(interfaceId: string, weigher = false) {
+    console.log('deviceConnected');
 
-        this.handleData(success);
-        this.showToast('Connected Successfullly');
-      },
-      // error => {
-      //   this.showError(error);
-      // },
-      address
-    );
+    if (weigher) {
+      // Subscribe to data receiving as soon as the delimiter is read
+      this.bluetoothSerial.subscribe(
+        interfaceId,
+        '\r',
+        data => {
+          const bytes = new Uint8Array(data);
+
+          this.handleData(data);
+          console.log('data', data);
+          console.log('bytes', bytes);
+          this.showToast('Connected Successfully');
+        },
+        error => {
+          this.showError(error);
+        }
+      );
+    } else {
+      // Subscribe to data receiving as soon as the delimiter is read
+      this.bluetoothSerial.subscribe(
+        interfaceId,
+        '\r',
+        data => {
+          const bytes = new Uint8Array(data);
+
+          this.handleData(data);
+          console.log('data', data);
+          console.log('bytes', bytes);
+          this.showToast('Connected Successfully');
+        },
+        error => {
+          this.showError(error);
+        }
+      );
+    }
   }
-
-  deviceConnected2(address) {
-    // Subscribe to data receiving as soon as the delimiter is read
-    this.bluetoothSerial.subscribe2(
-      '\r',
-      success => {
-        var bytes = new Uint8Array(success);
-
-        this.handleData(success);
-        this.showToast('Connected Successfullly');
-      },
-      // error => {
-      //   this.showError(error);
-      // },
-      address
-    );
-  }
-
-  deviceConnected3(address) {
-    // Subscribe to data receiving as soon as the delimiter is read
-    this.bluetoothSerial.subscribe3(
-      '\r',
-      success => {
-        var bytes = new Uint8Array(success);
-
-        this.handleData(success);
-        this.showToast('Connected Successfullly');
-      },
-      // error => {
-      //   this.showError(error);
-      // },
-      address
-    );
-  }
-
 
   /* Handle Data */
   handleData(data) {
     this.showToast(data);
-
-    // let messageText = "\n" + data;
     this.message += data;
   }
 
+  // /* Disconnect */
+  // disconnect() {
+  //   this.showConnectReader = true;
+  //   this.showDisconnectReader = false;
 
-  /* Disconnect */
-  disconnect() {
-    this.showConnectReader = true;
-    this.showDisconnectReader = false;
+  //   let alert = this.alertCtrl.create({
+  //     title: 'Disconnect?',
+  //     message: 'Do you want to Disconnect?',
+  //     buttons: [
+  //       {
+  //         text: 'Cancel',
+  //         role: 'cancel',
+  //         handler: () => {
+  //           alert.dismiss();
+  //         }
+  //       },
+  //       {
+  //         text: 'Disconnect',
+  //         handler: () => {
+  //           this.bluetoothSerial.disconnect(success => {}, error => {}, this.address1);
+  //         }
+  //       }
+  //     ]
+  //   });
+  //   alert.present();
+  // }
 
-    let alert = this.alertCtrl.create({
-      title: 'Disconnect?',
-      message: 'Do you want to Disconnect?',
-      buttons: [
-        {
-          text: 'Cancel',
-          role: 'cancel',
-          handler: () => {
-            alert.dismiss();
-          }
-        },
-        {
-          text: 'Disconnect',
-          handler: () => {
-            this.bluetoothSerial.disconnect(success => {}, error => {}, this.address1);
-            this.gettingDevices = null;
-          }
-        }
-      ]
-    });
-    alert.present();
-  }
+  private write(data, interfaceId?: string) {
+    let osSpecificInterfaceId;
+    if (this.platform.is('android')) {
+      osSpecificInterfaceId = '00001101-0000-1000-8000-00805F9B34FB';
+    } else if (this.platform.is('ios')) {
+      osSpecificInterfaceId = interfaceId;
+    } else {
+      alert('not supported');
+      return;
+    }
 
-  disconnect2() {
-    this.showConnectWeigher = true;
-    this.showDisconnectWeigher = false;
-
-    let alert = this.alertCtrl.create({
-      title: 'Disconnect?',
-      message: 'Do you want to Disconnect?',
-      buttons: [
-        {
-          text: 'Cancel',
-          role: 'cancel',
-          handler: () => {
-            alert.dismiss();
-          }
-        },
-        {
-          text: 'Disconnect',
-          handler: () => {
-            this.bluetoothSerial.disconnect2(success => {}, error => {}, this.address2);
-            this.gettingDevices = null;
-          }
-        }
-      ]
-    });
-    alert.present();
-  }
-
-  disconnect3() {
-    this.showConnectBarcode = true;
-    this.showDisconnectBarcode = false;
-
-    let alert = this.alertCtrl.create({
-      title: 'Disconnect?',
-      message: 'Do you want to Disconnect?',
-      buttons: [
-        {
-          text: 'Cancel',
-          role: 'cancel',
-          handler: () => {
-            alert.dismiss();
-          }
-        },
-        {
-          text: 'Disconnect',
-          handler: () => {
-            this.bluetoothSerial.disconnect3(success => {}, error => {}, this.address3);
-            this.gettingDevices = null;
-          }
-        }
-      ]
-    });
-    alert.present();
-  }
-
-
-  next() {
-    this.navCtrl.push('TerminalPage');
-  }
-
-  enableCarriage(address) {
-    this.dataSend = '{ZC1}'; // enable carriage \n
-    this.showToast(this.dataSend);
-
-    this.bluetoothSerial.write2(
-      this.dataSend,
+    this.bluetoothSerial.write(
+      osSpecificInterfaceId,
+      data,
       success => {
         this.showToast(success);
       },
       error => {
         this.showError(error);
-      },
-      address
+      }
     );
   }
 
-  sendData() {
-    this.enableCarriage(this.address2);
-
-    this.dataSend = '{RW}'; // trutest - yes  gallagher?   this is a live weight
-    this.showToast(this.dataSend);
-
-    this.bluetoothSerial.write2(
-      this.dataSend,
-      success => {
-        this.showToast(success);
-      },
-      error => {
-        this.showError(error);
-      },
-      this.address2
-    );
+  requestWeight() {
+    this.write(this.CARRIAGE_DATA, this.weigherConfig.protocols[0]); // enable carriage \n
+    this.write('{RW}', this.weigherConfig.protocols[0]); // Tru-Test - yes  gallagher?   this is a live weight
   }
 
-  sendData1() {
-    this.enableCarriage(this.address2);
-
-    this.dataSend = '{RO}'; // trutest - yes  gallagher?   this is a stable weight
-    // this.dataSend ='{RS}'; // trutest - yes  gallagher?   this is a stable weight
-    this.showToast(this.dataSend);
-
-    this.bluetoothSerial.write2(
-      this.dataSend,
-      success => {
-        this.showToast(success);
-      },
-      error => {
-        this.showError(error);
-      },
-      this.address2
-    );
+  requestStableWeight() {
+    this.write(this.CARRIAGE_DATA, this.weigherConfig.protocols[0]); // enable carriage \n
+    this.write('{RO}', this.weigherConfig.protocols[0]); // Tru-Test - yes  gallagher?   this is a live weight
   }
-
-  page() {}
 
   showError(error) {
     let alert = this.alertCtrl.create({
@@ -414,19 +266,4 @@ export class HomePage implements OnInit {
     });
     toast.present();
   }
-
-  adjust(): void {
-    const textArea = this.element.nativeElement.getElementsByTagName('textarea')[0];
-    textArea.style.overflow = 'hidden';
-    textArea.style.height = 'auto';
-    textArea.style.height = textArea.scrollHeight + 'px';
-    textArea.style.fontSize = '12px';
-  }
-}
-
-interface pairedlist {
-  class: number;
-  id: string;
-  address: string;
-  name: string;
 }
